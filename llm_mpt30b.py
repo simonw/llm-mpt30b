435a05dc8e12f681fff05c0882aa8678c5f7df23
Simon Willison <swillison@gmail.com>
2023-07-10 08:06:37 -0700

    Renamed iter_prompt to execute
    Refs https://github.com/simonw/llm/pull/65#issuecomment-1629127101

--- llm_mpt30b.py
+++ llm_mpt30b.py
@@ -1,8 +1,6 @@
-import click
 from ctransformers import AutoModelForCausalLM, AutoConfig
 from huggingface_hub import hf_hub_download
 import llm
-import llm.cli
 import os
 
 from tqdm import tqdm
@@ -35,53 +33,52 @@ class Mpt30b(llm.Model):
     class Options(llm.Options):
         verbose: bool = False
 
-    class Response(llm.Response):
-        def iter_prompt(self, prompt):
-            original_init = tqdm.__init__
-            if not prompt.options.verbose:
-                # Disable all tqdm output, using this workaround:
-                # https://stackoverflow.com/a/67238486/6083
-                tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
-            try:
-                llm_model = AutoModelForCausalLM.from_pretrained(
-                    "TheBloke/mpt-30B-chat-GGML",
-                    model_type="mpt",
-                    model_file="mpt-30b-chat.ggmlv0.q4_1.bin",
-                    config=AutoConfig.from_pretrained(
-                        "mosaicml/mpt-30b-chat", context_length=8192
-                    ),
-                    local_files_only=True,
-                )
+    def execute(self, prompt, stream, response):
+        original_init = tqdm.__init__
+        if not prompt.options.verbose:
+            # Disable all tqdm output, using this workaround:
+            # https://stackoverflow.com/a/67238486/6083
+            tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
+        try:
+            llm_model = AutoModelForCausalLM.from_pretrained(
+                "TheBloke/mpt-30B-chat-GGML",
+                model_type="mpt",
+                model_file="mpt-30b-chat.ggmlv0.q4_1.bin",
+                config=AutoConfig.from_pretrained(
+                    "mosaicml/mpt-30b-chat", context_length=8192
+                ),
+                local_files_only=True,
+            )
 
-                system_prompt = (
-                    "A conversation between a user and an LLM-based AI assistant."
-                )
+            system_prompt = (
+                "A conversation between a user and an LLM-based AI assistant."
+            )
 
-                prompt_lines = [
-                    # System prompt
-                    f"<|im_start|>system\n{system_prompt}<|im_end|>\n",
-                    # User prompt
-                    f"<|im_start|>user\n{prompt.prompt}<|im_end|>\n",
-                    # Start of assistant prompt
-                    f"<|im_start|>assistant\n",
-                ]
+            prompt_lines = [
+                # System prompt
+                f"<|im_start|>system\n{system_prompt}<|im_end|>\n",
+                # User prompt
+                f"<|im_start|>user\n{prompt.prompt}<|im_end|>\n",
+                # Start of assistant prompt
+                f"<|im_start|>assistant\n",
+            ]
 
-                self._prompt_json = {"prompt_lines": prompt_lines}
+            response._prompt_json = {"prompt_lines": prompt_lines}
 
-                generator = llm_model(
-                    "".join(prompt_lines),
-                    temperature=0.2,
-                    top_k=0,
-                    top_p=0.9,
-                    repetition_penalty=1.0,
-                    max_new_tokens=512,  # adjust as needed
-                    seed=42,
-                    reset=False,  # reset history (cache)
-                    stream=True,  # streaming per word/token
-                    threads=int(os.cpu_count() / 2),  # adjust for your CPU
-                    stop=["<|im_end|>", "|<"],
-                )
-                for word in generator:
-                    yield word
-            finally:
-                tqdm.__init__ = original_init
+            generator = llm_model(
+                "".join(prompt_lines),
+                temperature=0.2,
+                top_k=0,
+                top_p=0.9,
+                repetition_penalty=1.0,
+                max_new_tokens=512,  # adjust as needed
+                seed=42,
+                reset=False,  # reset history (cache)
+                stream=True,  # streaming per word/token
+                threads=int(os.cpu_count() / 2),  # adjust for your CPU
+                stop=["<|im_end|>", "|<"],
+            )
+            for word in generator:
+                yield word
+        finally:
+            tqdm.__init__ = original_init
